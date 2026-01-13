5bcf3a76cf68e190f537832af1df7ee2c8d62cbb
aponyavina <anna.ponyavina@brotherhood.software>
2026-01-13 21:55:50 +0300

    br-2: test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log(222)
+  console.log(2222)
   return (
     <div className="App">
       <header className="App-header">
